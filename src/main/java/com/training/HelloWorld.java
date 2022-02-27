08ce62f5398dcdafd398e09a7a52c81cebc79f57
Mona Lisa <bochenek.patryk1992@gmail.com>
2022-02-27 09:52:24 +0100

    text change

--- src/main/java/com/training/HelloWorld.java
+++ src/main/java/com/training/HelloWorld.java
@@ -3,7 +3,7 @@ package com.training;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Witaj Swiecie!");
+        System.out.println("Witaj Patryk!");
     }
 }
 
